009428cce231c9b5a51a576bb3745cd879ab5d2b
ganeev_andrei <ganeevandrej21@gmail.com>
2024-01-22 22:10:18 +0300

    Using file-loader(image, fonts) and css-loader(css and css modules)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import img from "./logo.png";
+import styles from "./index.module.css";
+import "./main.css";
 
 const App = () => {
     console.log([1, 2, [3, 4]].flat());
-    return <h1>Hello Andrey</h1>
+    return (
+        <div className={styles.wrapper}>
+            <h1>Hello Andrey</h1>
+            <img src={img} />
+        </div>
+    );
 };
 
 const root = createRoot(document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,45 @@ module.exports = {
               loader: 'babel-loader'
             }
           ]
-        }
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|ico)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'images',
+                name: '[name]-[sha1:hash:7].[ext]'
+              }
+            }
+          ]
+        },
+        {
+          test: /\.(ttf|otf|eot|woff|woff2)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'fonts',
+                name: '[name].[ext]'
+              }
+            }
+          ]
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: 'style-loader'
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+              },
+            },
+          ]
+        },
       ]
     }
 }
